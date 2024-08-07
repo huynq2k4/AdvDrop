87359210092413c09c81512c2c2775bcbda40ccc
huynq2k4 <huynq.contact@gmail.com>
2024-08-07 08:54:33 +0700

    Change a little bit in description of pbar

--- run_AdvDrop.py
+++ run_AdvDrop.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
                     # avg_inv_loss_adp += inv_loss.detach().item()
                     avg_inv_loss_adp += 0
                     num_batches_adp += 1
-                    pbar.set_description(f"Epoch_adv {epoch_adv}/{args.adv_epochs}:")
+                    pbar.set_description(f"Epoch_adv {epoch_adv + 1}/{args.adv_epochs}:")
 
                 t2 = time.time()
                 perf_str = 'Adv Epoch %d [%.1fs]: adjust avg inv == %.5f' % (
